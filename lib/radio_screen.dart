aebdc19042728c89845457e5cf2a4d0b8fdcb183
Mohameed-Asad <mohamedasadn19@gmail.com>
2024-02-13 07:12:18 +0200

    finish radio and settings feature

--- lib/radio_screen.dart
+++ lib/radio_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:islamii_app/theme_manager.dart';
 
 class RadioScreen extends StatelessWidget {
   const RadioScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Container(
+            height: 350,
+            child: Image.asset(
+              "assets/Images/radio_image.png",
+              height: MediaQuery.of(context).size.height * 0.25,
+              alignment: Alignment.bottomCenter,
+            )),
+        Padding(
+          padding: const EdgeInsets.only(top: 40, bottom: 20),
+          child: Text(
+            "إذاعة القرآن الكريم",
+            style: ThemeDataManager.primaryStyle.copyWith(fontSize: 30),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_left,
+                color: ThemeDataManager.primaryColor,
+                size: 80,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.pause,
+                color: ThemeDataManager.primaryColor,
+                size: 70,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.play_arrow,
+                color: ThemeDataManager.primaryColor,
+                size: 50,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -1,10 +1,55 @@
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
+import 'package:islamii_app/theme_manager.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  List<String> language = ["عربي", "English"];
+  List<String> themeList = ["Light", "Dark"];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(top: 70, right: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "اللغة",
+            style: ThemeDataManager.primaryStyle.copyWith(
+              fontSize: 30,
+            ),
+            textAlign: TextAlign.end,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, top: 15, bottom: 25),
+            child: CustomDropdown(
+              items: language,
+              onChanged: (value) {},
+            ),
+          ),
+          Text(
+            "السمة",
+            style: ThemeDataManager.primaryStyle.copyWith(
+              fontSize: 30,
+            ),
+            textAlign: TextAlign.end,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, top: 15),
+            child: CustomDropdown(
+              items: themeList,
+              onChanged: (value) {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
